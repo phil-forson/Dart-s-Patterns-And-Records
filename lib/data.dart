432b5a4b5d232c61798e41bccac1de4cf1bde215
Philemon Forson <forsonphilemon1@gmail.com>
2024-09-12 13:18:09 +0000

    using patterns to extract data

--- lib/data.dart
+++ lib/data.dart
@@ -4,11 +4,30 @@ class Document {
   final Map<String, Object?> _json;
   Document() : _json = jsonDecode(documentJson);
 
-  (String, {DateTime modified}) get metadata {           
-    const title = 'My Document';
-    final now = DateTime.now();
+  (String, {DateTime modified}) get metadata {
+    if (_json                                                // Modify from here...
+        case {
+          'metadata': {
+            'title': String title,
+            'modified': String localModified,
+          }
+        }) {
+      return (title, modified: DateTime.parse(localModified));
+    } else {
+      throw const FormatException('Unexpected JSON');
+    }          
 
-    return (title, modified: now);
+    if (_json.containsKey('metadata')) {                     // Modify from here...
+      final metadataJson = _json['metadata'];
+      if (metadataJson is Map) {
+        final title = metadataJson['title'] as String;
+        final localModified =
+            DateTime.parse(metadataJson['modified'] as String);
+        return (title, modified: localModified);
+      }
+    }
+    throw const FormatException('Unexpected JSON'); 
+
   }       
 }
 
